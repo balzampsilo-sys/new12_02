303647ccb6624c7f45f5d3d8d7f4069e14d74ef2
balzampsilo-sys <balzampsilo@gmail.com>
2026-02-12 22:01:13 +0300

    feat: add awaiting_slot_interval state (3/4)
    Add new FSM state for slot interval configuration

--- utils/states.py
+++ utils/states.py
@@ -1,18 +1,18 @@
-"""FSM состояния для бота"""
+"""Состояния FSM для бота"""
 
 from aiogram.fsm.state import State, StatesGroup
 
 
 class BookingStates(StatesGroup):
-    """States для процесса бронирования"""
+    """Состояния для процесса бронирования"""
     waiting_for_date = State()
     waiting_for_time = State()
     waiting_for_confirmation = State()
 
 
 class AdminStates(StatesGroup):
-    """States для админ-панели"""
-    awaiting_broadcast = State()
+    """Состояния для админ-панели"""
+    awaiting_broadcast_message = State()  # ✅ УНИФИЦИРОВАНО имя
     awaiting_block_date = State()
     awaiting_block_time = State()
     awaiting_block_reason = State()
@@ -20,6 +20,7 @@ class AdminStates(StatesGroup):
     awaiting_removal_id = State()
     awaiting_work_hours_start = State()
     awaiting_work_hours_end = State()
+    awaiting_slot_interval = State()  # ✅ НОВОЕ состояние!
     
     # Календарные состояния
     reschedule_select_date = State()
